1d8ecf8b752cfb7f95f48f098da8e01367adfd39
Steven Riley <rileystevend@gmail.com>
2018-11-07 19:26:33 -0600

    adding api calls

--- src/translation/transcript.js
+++ src/translation/transcript.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
 
-const headers = {};
-const data = {};
-axios.post('https://api.rev.ai/revspeech/v1beta/jobs', data, headers)
+const headers = {
+    'Authorization': 'Bearer 01ovKCI58Pm2ZnVO9dXbZ_I7yEGlAgIlrWDpOR6K3Kejpp6nrBkS7iML5hmMPNdWH8ggVdQ7bEsuuUijPCzt7--ztqEQM',
+    'Content-Type': 'application/json'};
+
+const data = {
+        "media_url": "https://support.rev.com/hc/en-us/article_attachments/200043975/FTC_Sample_1_-_Single.mp3",
+        "metadata": "This is a sample submit jobs option",
+        // "callback_url": "https://www.example.com/callback"   
+};
+
+axios.post('https://api.rev.ai/revspeech/v1beta/jobs', data, {headers: headers})
   .then(function (response) {
     console.log(response);
   })
   .catch(function (error) {
     console.log(error);
   });
+
--- src/translation/translate.js
+++ src/translation/translate.js
@@ -4,6 +4,9 @@ const {Translate} = require('@google-cloud/translate');
 // Creates a client
 const translate = new Translate();
 
+const text = `Speaker 0    00:00    Hi, my name is Jack Ratzinger and I'm going to be talking about how to recruit a co founder and a team for your startup to first give a quick overview of what we're going to talk about. We're going to start out by talking about the sorts of folks you might be looking for for your startup, and then we'll dive right into tactics and approaches you can use to get inbound interest in your company. We'll talk a little bit about how people get, how to get people excited for your startup, and then we'll talk about the screening process, how to figure out if someone is a good match and someone you should make an offer to. Will then cover how you make an offer and we'll wrap up by covering a few specific issues. One is technical hiring, which can be a little different from more general recruiting, and finally we'll talk about specifically what you should do when you're looking for a cofounder.  
+Speaker 0    00:43    Many of the things we're going to be talking about are applicable to both finding a cofounder and finding employees, but there are a few specific issues that are relevant to when you're looking for a cofounder to be a part of your startup. So we'll talk about that. I want to give you a quick background on myself. I am the CO founder of <inaudible>. We are a search engine for sports and concert tickets based in New York City. This is my third startup and for all three we built a team so I have some experience with this at seatgeek. We have 17 folks and I think we have an incredible team and I want to talk about some things that we've done that I think has helped us get there. Um, it's sort of the overall thesis for this. One thing that's worth keeping in mind is that recruiting is a lot of work. Some people think that you can raise money and spend a few weeks building your team and then move on to more interesting things. It's totally not the case. Recruiting something that you're always doing. I as a cofounder, probably spend regularly 30 percent of my time trying to find new people to add to our team, but if you know it's. It's a lot of work because it's such an important thing to assess the success of a startup and if you invest that time, then the dividends pay off.` 
+const = ''
 /**
  * TODO(developer): Uncomment the following lines before running the sample.
  */
@@ -17,14 +20,12 @@ translate
   .translate(text, target)
   .then(results => {
     let translations = results[0];
-    translations = Array.isArray(translations)
-      ? translations
-      : [translations];
+    // translations = Array.isArray(translations) ? translations  : [translations];
 
-    console.log('Translations:');
-    translations.forEach((translation, i) => {
-      console.log(`${text[i]} => (${target}) ${translation}`);
-    });
+    // console.log('Translations:');
+    // translations.forEach((translation, i) => {
+    //   console.log(`${text[i]} => (${target}) ${translation}`);
+    // });
   })
   .catch(err => {
     console.error('ERROR:', err);
